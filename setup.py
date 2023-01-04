59662272759d036fbd68adf6583220a1ba8ec778
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2023-01-04 20:18:00 +0000

    fix: Missing comma in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version="0.0.6",
 	description="This is a unofficial wrapper for Vinter.co API",
 	long_description=open('Readme.md').read(),
-	long_description_content_type='text/markdown'
+	long_description_content_type='text/markdown',
 	install_requires=["requests==2.28.1"],
 	author="Rahul Mistri",
 	author_email="rahulmistri1997@gmail.com",
