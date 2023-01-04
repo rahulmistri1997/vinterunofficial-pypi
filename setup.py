6469db69e4cfe0d15a2f642234dc8f612eb321a3
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2023-01-04 20:35:20 +0000

    update: Rename Readme.md to be compitable with setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
+long_description = open('README.md').read()
+
 setup(
 	name="vinterunofficial",
 	version="0.0.6",
 	description="This is a unofficial wrapper for Vinter.co API",
-	long_description=open('Readme.md').read(),
+	long_description=long_description,
 	long_description_content_type='text/markdown',
 	install_requires=["requests==2.28.1"],
 	author="Rahul Mistri",
