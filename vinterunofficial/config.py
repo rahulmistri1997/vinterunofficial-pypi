e35096423c4787b9352026af23a6d954af14dc96
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2023-03-17 12:34:14 +0000

    add: Nav configs

--- vinterunofficial/config.py
+++ vinterunofficial/config.py
@@ -16,11 +16,13 @@ class AssetType(Enum):
     MULTI_ASSET = "multi_assets"
     SINGLE_ASSET = "single_assets"
     STAKING_YIELD = "staking_yields"
+    NAV = "nav"
 
 class ActiveAssetType(Enum):
     MULTI_ASSET = "active_multi_assets"
     SINGLE_ASSET = "active_single_assets"
     STAKING_YIELD = "active_staking_yields"
+    NAV = "active_nav"
 
 class AssetUrl(Enum):
     MULTI_ASSET_REAL_TIME = {
@@ -43,6 +45,11 @@ class AssetUrl(Enum):
         "asset_type": AssetType.SINGLE_ASSET,
         "url": f"{APIBASE}/{AssetType.SINGLE_ASSET.value}_{FrequencyApiType.HOURLY.value}",
     }
+    NAV_HOURLY = {
+        "frequency": Frequency.HOURLY,
+        "asset_type": AssetType.NAV,
+        "url": f"{APIBASE}/{AssetType.NAV.value}_{FrequencyApiType.HOURLY.value}",
+    }
     MULTI_ASSET_DAILY = {
         "frequency": Frequency.DAILY,
         "asset_type": AssetType.MULTI_ASSET,
@@ -58,6 +65,11 @@ class AssetUrl(Enum):
         "asset_type": AssetType.STAKING_YIELD,
         "url": f"{APIBASE}/{AssetType.STAKING_YIELD.value}_{FrequencyApiType.DAILY.value}",
     }
+    NAV_DAILY = {
+        "frequency": Frequency.DAILY,
+        "asset_type": AssetType.NAV,
+        "url": f"{APIBASE}/{AssetType.NAV.value}_{FrequencyApiType.DAILY.value}",
+    }
     ACTIVE_MULTI_ASSET = {
         "frequency": None,
         "asset_type": AssetType.MULTI_ASSET,
@@ -72,4 +84,9 @@ class AssetUrl(Enum):
         "frequency": None,
         "asset_type": AssetType.STAKING_YIELD,
         "url": f"{APIBASE}/{ActiveAssetType.STAKING_YIELD.value}",
+    },
+    ACTIVE_NAV = {
+        "frequency": None,
+        "asset_type": AssetType.NAV,
+        "url": f"{APIBASE}/{ActiveAssetType.NAV.value}",
     }
