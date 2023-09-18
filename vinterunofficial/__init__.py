f1ab7f6103747cfd8211faaa9cad7c52b43a91f5
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2023-09-18 11:55:48 +0000

    add: Version in library

--- vinterunofficial/__init__.py
+++ vinterunofficial/__init__.py
@@ -2,3 +2,5 @@ from .vinter_sdk import VinterAPI
 from .vinter_sdk_async import VinterAPIAsync
 from .utils import VinterUrl, VinterValidation
 from .vinter_sdk_ws import VinterAPIWS
+
+__version__ = "0.1.8"
